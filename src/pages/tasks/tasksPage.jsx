11a56091cc3f0e1275065a6cdfe8955f393c115c
Daniel Barcelo <danieldeveloper11@gmail.com>
2023-01-28 14:30:43 -0500

    agregando configuraciones

---
+++ src/pages/tasks/tasksPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import TaskList from '../../components/container/taskList';
+
+const TasksPage = () => {
+    return (
+        <div>
+            <TaskList />
+        </div>
+    );
+}
+
+export default TasksPage;
